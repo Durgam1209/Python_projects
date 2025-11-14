d7a2f219cc5e8237bf2bb736642dbd2c482e258e
Durgam1209 <durga.m1209@gmail.com>
2025-11-14 21:20:42 +0530

    commit the folder

--- cli_calculator.py
+++ cli_calculator.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 #calculator
 
@@ -31,4 +32,39 @@ op1 = float(input("Enter first num: "))
 op = input("Enter any of operator\n[+,-,*,/,%]:")
 op2 = float(input("Enter second num: "))   
 
+=======
+
+#calculator
+
+def calculator(op1,op,op2):
+    res=0
+    def calculation(op1,op,op2):
+        if op == '+':
+            return float(op1+op2)
+        elif op == '-':
+            return op1-op2
+        elif op == '*':
+            return op1*op2
+        elif op == '/':
+            try:
+                res= op1/op2
+            except:
+                print("Cannot divide a number by Zero \n Try again withanother number")
+            else:
+                return res
+        elif op == '%':
+            try:
+                res= op1%op2
+            except:
+                print("Cannot divide a number by Zero \n Try again with another INTEGER number")
+            else:
+                return res
+    return calculation(op1,op,op2)
+
+print("-------------------Simple CLI calculator----------------------")
+op1 = float(input("Enter first num: "))
+op = input("Enter any of operator\n[+,-,*,/,%]:")
+op2 = float(input("Enter second num: "))   
+
+>>>>>>> 2532ad1 (commit the folder)
 print(f'{op1} {op} {op2} = {calculator(op1,op,op2)}')
